49d69f197f6ab4a932b20717251cb3d20df9ff1e
Lucas <luckofahl@gmail.com>
2023-09-02 15:41:32 -0300

    ajustes em voltar

--- rocketnotes/src/components/Header/index.jsx
+++ rocketnotes/src/components/Header/index.jsx
@@ -2,9 +2,17 @@ import { RiShutDownLine} from 'react-icons/ri';
 import { useAuth } from '../../hooks/auth'
 import { Conteiner, Profile, Logout } from "./styles";
 import { api } from "../../Service/api";
+import avatarPlaceholder from "../../Assets/avatar_placeholder.svg";
+import { useNavigate } from 'react-router-dom';
 
 export function Header(){
   const { signOut, user } = useAuth();
+  const navigate = useNavigate();
+  
+  function handleSignOut(){
+    navigate("/");
+    signOut();
+  }
 
   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/file/${user.avatar}` : avatarPlaceholder;
 
@@ -22,7 +30,7 @@ export function Header(){
         </div>
       </Profile>
 
-    <Logout onClick={signOut}>
+    <Logout onClick={handleSignOut}>
       <RiShutDownLine/>
     </Logout>
 
--- rocketnotes/src/pages/Profile/index.jsx
+++ rocketnotes/src/pages/Profile/index.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
-import { ButtonText } from '../../components/ButtonText';
 import { useAuth } from '../../hooks/auth';
 import { api } from '../../Service/api';
 import { useNavigate } from 'react-router-dom';
